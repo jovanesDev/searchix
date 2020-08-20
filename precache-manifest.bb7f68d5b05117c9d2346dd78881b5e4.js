f742732f4f9271df5fa2208ae199d5c9313be6a8
Jovaness <jovanes.web@gmail.com>
2020-08-19 23:27:57 -0300

    Updates

---
+++ precache-manifest.bb7f68d5b05117c9d2346dd78881b5e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0373663c5bf5619243d3a6e25b1eb9b",
+    "url": "/searchix/index.html"
+  },
+  {
+    "revision": "5de2e4d164e951660706",
+    "url": "/searchix/static/css/main.0832c3a7.chunk.css"
+  },
+  {
+    "revision": "dfdb95f1e65518eb0c5f",
+    "url": "/searchix/static/js/2.4a100061.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searchix/static/js/2.4a100061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de2e4d164e951660706",
+    "url": "/searchix/static/js/main.084707ac.chunk.js"
+  },
+  {
+    "revision": "153b82d270d2155527fa",
+    "url": "/searchix/static/js/runtime-main.2aaf6400.js"
+  },
+  {
+    "revision": "2b0ebe3f829e42877a49b0893edeaaff",
+    "url": "/searchix/static/media/banner.2b0ebe3f.jpg"
+  }
+]);
